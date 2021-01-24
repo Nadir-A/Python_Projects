4884092703143be2d5fc520b8f5b69f440c1ce3a
Nadir Ahmed <76652376+Nadir-A@users.noreply.github.com>
2021-01-24 00:59:06 +0000

    Updated functions.py
    Added date and time function

--- Bitcoin_USD_GBP/function.py
+++ Bitcoin_USD_GBP/function.py
@@ -1,5 +1,12 @@
+import datetime
 # bitcoin converter functions
+
+#-------- current date and time
+def time():
+    return datetime.datetime.now()
+
 
 #------ bitcoin-usd convert to bitcoin_gbp
 def convert_to_gbp(bitcoin_to_usd, usd_to_gbp):
     return round(bitcoin_to_usd * usd_to_gbp, 2) # round to 2 decimal places
+
